615a2ca1a7926f4b7d77209613a96748e03b1ee6
Андрей Каменко <bolognese.dev@gmail.com>
2022-06-10 17:34:44 +0300

    Созданы модели. Отредактированы шаблоны. Добавлены баги.

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Group, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    # Поля в админке
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group'
+    )
+
+    # Интерфейс изминения группы
+    list_editable = ('group',)
+    # Интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+    # Фильтрация по дате
+    list_filter = ('pub_date',)
+    # Пустое поле, если в графе ничего нет
+    empty_value_display = '-empty-'
+
+
+# Регистрация конфигуратора PostAdmin для модели Post
+admin.site.register(Post, PostAdmin)
+
+# Регистрация модели Group
+admin.site.register(Group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
 
 
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
     title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('pub_date')[:10]
     context = {
-        'title': title
+        'title': title,
+        'posts': posts
     }
+
     return render(request, template, context)
 
 
 def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
     context = {
-        'title': title
+        'group': group,
+        'posts': posts
     }
     return render(request, template, context)
